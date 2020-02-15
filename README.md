f78c5b108fa430c982589051b758dda84640370b
demkantor <demkantor@gmail.com>
2020-02-15 17:46:53 -0600

    add task function complete from DOM to database to DOM display

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # weekend_to_do_app
+
+
+Icons made by "https://www.flaticon.com/authors/freepik"
+
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -16,6 +16,21 @@ router.get('/', (req,res)=>{
     })
 })//end GET tasks
 
+//add new task to database
+router.post('/', (req,res)=>{
+    console.log('in /tasks POST:', req.body);
+    const query = `INSERT INTO "tasks" ("name") VALUES ($1);`;
+    const values = [req.body.name];
+    pool.query(query, values).then((results)=>{
+        res.sendStatus(201);
+    }).catch((err)=>{
+        console.log('error with INSERT', err);
+        res.sendStatus(500);
+    })
+})//end POST tasks
+
+
+
 
 
 //exports

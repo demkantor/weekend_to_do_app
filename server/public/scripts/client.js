3f73a00904119e8f69d54a6345063fa5fd3e4802
demkantor <demkantor@gmail.com>
2020-02-15 14:47:35 -0600

    on page load tasks from database displayed to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,25 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('hit it!');
+    displayList();
+}
+
+function displayList(){
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function (response){
+        console.log(response);
+        let el = $('.taskList');
+        el.empty();
+        for(let i=0; i<response.length; i++){
+            el.append(`<div class="box tasks" data-id="${response[i].id}">
+            <button class="completeMe" data-completed="${response[i].completed}">C</button>
+            ${response[i].name}
+            <button class="deleteMe" data-id="${response[i].id}">D</button>
+            </div>`)
+        }
+    }).catch(function(err){
+        alert('Error getting Task List', err);
+    })
 }
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -3,7 +3,18 @@ const pool = require('../modules/pool.js');
 const router = express.Router();
 
 //routes
-
+//get full list of tasks from database
+router.get('/', (req,res)=>{
+    console.log('in /tasks GET');
+    const query = `SELECT * FROM "tasks";`;
+    pool.query(query).then((results)=>{
+        console.table(results.rows);
+        res.send(results.rows);
+    }).catch((err)=>{
+        console.log('ERROR with /tasks GET', err);
+        res.sendStatus(500);
+    })
+})//end GET tasks
 
 
 

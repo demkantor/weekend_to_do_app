0f2f3c852c8639a90f871e4bcd7ff73feccd29cd
demkantor <demkantor@gmail.com>
2020-02-15 15:14:18 -0600

    capture clicks on completed and delete buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,19 @@ $(document).ready(onReady);
 function onReady(){
     console.log('hit it!');
     displayList();
+    $('.taskList').on('click', '.deleteMeButton', deleteMe);
+    $('.taskList').on('click', '.completeMeButton', completeMe);
+}
+
+function completeMe(){
+    let selectedId = $(this).parent().data('id');
+    let completed = $(this).data('completed');
+    console.log('in completeMe with id:', selectedId, completed);
+}
+
+function deleteMe(){
+    let selectedId = $(this).data('id');
+    console.log('in deleteMe with id:', selectedId);
 }
 
 function displayList(){
@@ -17,9 +30,9 @@ function displayList(){
         el.empty();
         for(let i=0; i<response.length; i++){
             el.append(`<div class="box tasks" data-id="${response[i].id}">
-            <button class="completeMe" data-completed="${response[i].completed}">C</button>
+            <button class="completeMeButton" data-completed="${response[i].completed}">C</button>
             ${response[i].name}
-            <button class="deleteMe" data-id="${response[i].id}">D</button>
+            <button class="deleteMeButton" data-id="${response[i].id}">D</button>
             </div>`)
         }
     }).catch(function(err){
